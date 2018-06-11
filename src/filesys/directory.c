17e595b16139f77080b1ad09f347059a7daa9036
Taehoon Moon <panarch@kaist.ac.kr>
2018-06-11 09:56:01 +0000

    Update directory not to allow remove if directory is already opened

--- src/filesys/directory.c
+++ src/filesys/directory.c
@@ -213,6 +213,10 @@ dir_remove (struct dir *dir, const char *name)
           dir_close (target_dir);
           goto done;
         }
+
+      /* Check inode is not being used */
+      if (inode_get_open_cnt (inode) > 1)
+        goto done;
     }
 
   /* Erase directory entry. */
